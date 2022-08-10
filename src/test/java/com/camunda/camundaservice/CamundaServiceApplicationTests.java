0ffd9632151779f3cd34e6247c68cc18bfa58b46
Serdar Yazıcı <serdar.yazici@eteration.com>
2022-08-10 20:18:06 +0300

    initialize spring boot project

---
+++ src/test/java/com/camunda/camundaservice/CamundaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camunda.camundaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
