2bdebc4280c9b2eba463475baad2bcefb09e7431
Serdar Yazıcı <serdar.yazici@eteration.com>
2022-08-10 23:11:12 +0300

    camunda spring boot settings

--- src/main/java/com/camunda/camundaservice/CamundaServiceApplication.java
+++ src/main/java/com/camunda/camundaservice/CamundaServiceApplication.java
@@ -1,9 +1,11 @@
 package com.camunda.camundaservice;
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class CamundaServiceApplication {
 
 	public static void main(String[] args) {
